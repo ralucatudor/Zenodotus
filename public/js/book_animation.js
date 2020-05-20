fd8c1e0524cbfe801dff740ea3162d661d8a0a35
Raluca <tudorr30@gmail.com>
2020-05-20 22:12:53 +0300

    added form css, refactored code

---
+++ public/js/book_animation.js
@@ -0,0 +1,13 @@
+var book_animation_html_code =
+    `<span class="page turn"></span>
+    <span class="page turn"></span>
+    <span class="page turn"></span>
+    <span class="page turn"></span>
+    <span class="page turn"></span>
+    <span class="page turn"></span>
+    <span class="cover"></span>
+    <span class="page"></span>
+    <span class="cover turn"></span>`;
+
+var book_animation_container = document.getElementById('book_animation');
+book_animation_container.innerHTML = book_animation_html_code;
